dd70afd5457faa4b9ba9d36839414d1ba7ae01e4
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-14 02:08:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  beginning = 2
+  if number > 1
+    num_range = (beginning..number-1).to_a
+    num_range.none? do |prime| 
+      number % prime == 0
+    end
+  else
+    false
+  end
+end
